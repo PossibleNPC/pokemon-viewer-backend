4f70655458520fd14dc727f23e9a538ae0537c1d
PossibleNPC <28771786+PossibleNPC@users.noreply.github.com>
2022-11-27 00:20:25 +0100

    docker builder and testing env

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func getApiPort() string {
+	return os.Getenv("API_PORT")
+}
 
 func main() {
 	r := gin.Default()
+	PORT := getApiPort()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":" + PORT) // listen and serve on 0.0.0.0:8080
 }
